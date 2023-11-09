4e114d77d5784db71c89f45f07be662ea49506b3
Sourabh Jain <jainsourabh2@users.noreply.github.com>
2023-11-09 13:49:46 +0530

    Update 3-network.tf

--- modules/3-network.tf
+++ modules/3-network.tf
@@ -1,13 +1,13 @@
 resource "google_compute_network" "vpc_network" {
   project                 = google_project.terraform_generated_project.project_id
-  name                    = "dream11-hackathon"
+  name                    = "hackathon-vpc"
   auto_create_subnetworks = false
   mtu                     = 1460
   depends_on = [google_project_service.enable_api]
 }
 
 resource "google_compute_subnetwork" "vpc_subnetwork" {
-  name          = "terraform-sub-network-logistics-demo"
+  name          = "terraform-subnetwork-hackathon-vpc"
   ip_cidr_range = "10.0.0.0/24"
   region        = var.region
   private_ip_google_access = true
